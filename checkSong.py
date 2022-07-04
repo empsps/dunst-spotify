740aa7ec3fd7e4788ceb1292d3270b238f5de0bf
Sophia <sophialealbarros@gmail.com>
2022-07-04 20:52:27 -0300

    fix: correct dir for saving album cover

--- checkSong.py
+++ checkSong.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
 
-from json import dumps, load, dump
+from re import sub
 from os import makedirs
+from time import sleep, time
 from os.path import exists, join
+from json import dumps, load, dump
 from subprocess import check_output
 from urllib.request import urlretrieve
-from re import sub
 
-currentDir = '/home/sph/Dev/dunspotify'
-# cacheDir = "/home/sph/.local/cache/dunspotify"
-cacheDir = '/home/sph/Dev/dunspotify'
-lastSavedSongFile = join(cacheDir, 'lastSavedSong.json')
-currentSongFile = join(cacheDir, 'currentSong.json')
+current_dir = '/home/sph/Dev/dunspotify'
+cache_dir = '/home/sph/.local/share/dunspotify'
+last_saved_song_file = join(cache_dir, 'lastSavedSong.json')
+current_song_file = join(cache_dir, 'currentSong.json')
 
 # empty json structure to populate files
-songStructure = {
+song_structure = {
     'coverUrl': '',
     'songTitle': '',
     'artist': '',
     'albumTitle': ''
 }
 
-jsonStructure = dumps(songStructure)
+json_song_structure = dumps(song_structure)
 
 
 # create files if they don't exist
 def create_files_dirs():
-    if not exists(cacheDir):
+    if not exists(cache_dir):
         print('Creating dirs')
-        makedirs(cacheDir)
+        makedirs(cache_dir)
 
-    files = [lastSavedSongFile, currentSongFile]
+    files = [last_saved_song_file, current_song_file]
 
     for file in files:
         if not exists(file):
             with open(file, 'w') as file:
-                file.write(jsonStructure)
+                file.write(json_song_structure)
 
 
 def get_metadata():
@@ -45,24 +45,24 @@ def get_metadata():
 
 
 def convert_metadata_json(metadata):
-    coverUrl = ''
-    songTitle = ''
+    cover_url = ''
+    song_title = ''
     album = ''
     artist = ''
 
     for line in metadata.split('\n'):
         if 'artUrl' in line:
-            coverUrl = line.split('|')[1]
+            cover_url = line.split('|')[1]
         if 'title' in line:
-            songTitle = line.split('|')[1]
+            song_title = line.split('|')[1]
         if 'album' in line:
             album = line.split('|')[1]
         if 'artist' in line:
             artist = line.split('|')[1]
 
     readyData = {
-        'coverUrl': coverUrl,
-        'songTitle': songTitle,
+        'coverUrl': cover_url,
+        'songTitle': song_title,
         'album': album,
         'artist': artist
     }
@@ -72,62 +72,64 @@ def convert_metadata_json(metadata):
 
 # format the album title to give it as a name to the downloaded album cover
 def format_album_title():
-    albumTitleFormatted = ''
-    with open(currentSongFile, 'r') as current:
+    album_title_formatted = ''
+    with open(current_song_file, 'r') as current:
         # get album title from current song file and make it lowercase
-        albumTitleFormatted = load(current)['album'].lower()
+        album_title_formatted = load(current)['album'].lower()
 
     # remove all characters that ARE NOT letters and numbers
-    albumTitleFormatted = sub('[^ a-zA-Z0-9]', '', albumTitleFormatted)
+    album_title_formatted = sub('[^ a-zA-Z0-9]', '', album_title_formatted)
     # replace spaces for underscores
-    albumTitleFormatted = albumTitleFormatted.replace(' ', '_')
+    album_title_formatted = album_title_formatted.replace(' ', '_')
 
-    return albumTitleFormatted
+    return album_title_formatted
 
 
 def download_album_cover():
-    albumTitleFormatted = format_album_title()
-    coverUrl = ''
+    album_title_formatted = format_album_title()
+    cover_url = ''
 
-    with open(currentSongFile, 'r') as current:
-        coverUrl = load(current)['coverUrl'].lower()
+    with open(current_song_file, 'r') as current:
+        cover_url = load(current)['coverUrl'].lower()
 
     # check if the album cover was previously downloaded
-    if exists(join(cacheDir, albumTitleFormatted + '.png')):
+    if exists(join(cache_dir, album_title_formatted + '.png')):
         print('Album cover already saved, no need to download it again.')
         return
 
     # download the album cover and name it to the formatted album title
-    urlretrieve(coverUrl, albumTitleFormatted + '.png')
-    print(f'Downloaded {albumTitleFormatted}.png')
+    urlretrieve(cover_url, join(cache_dir, album_title_formatted) + '.png')
+    print(f'Downloaded {album_title_formatted}.png')
 
 
 # get currently playing song's metadata and save it to the file
 def write_song_to_file():
     # retrieve currently playing song's metadata
-    newSong = get_metadata()
+    new_song = get_metadata()
     # read last saved and current song files
-    with open(lastSavedSongFile, 'w') as last, open(currentSongFile, 'r+') as current:
+    with open(last_saved_song_file, 'w') as last, open(current_song_file, 'r+') as current:
 
         # copy current file contents to last saved
-        currentData = load(current)
-        dump(currentData, last)
+        current_data = load(current)
+        dump(current_data, last)
 
         # write real current song to current file
         current.seek(0)
-        current.write(newSong)
+        current.write(new_song)
         current.truncate()
+
+    compare_songs()
 
 
 # check if current song is in the same album as last saved song
 def compare_songs():
     print('Comparing last saved song with current')
-    with open(lastSavedSongFile, 'r') as last, open(currentSongFile, 'r') as current:
+    with open(last_saved_song_file, 'r') as last, open(current_song_file, 'r') as current:
 
         # read both files
-        coverUrlLast = load(last)['coverUrl']
-        coverUrlCurrent = load(current)['coverUrl']
-        if coverUrlLast == coverUrlCurrent:
+        cover_url_last = load(last)['coverUrl']
+        cover_url_current = load(current)['coverUrl']
+        if cover_url_last == cover_url_current:
             # if album cover url matches, assume it's in the same album
             print('Still in the same album as last song')
             return
@@ -135,16 +137,18 @@ def compare_songs():
             # if not, download the album cover
             print('Downloading album cover...')
             download_album_cover()
-            pass
+
+
+def song_check_loop():
+    start_time = time()
+    while True:
+        write_song_to_file()
+        sleep(6.0 - ((time() - start_time) % 6.0))
 
 
 def main():
-    # create_files_dirs()
-    # convert_metadata_json()
-    # download_album_cover()
-    # get_metadata()
-    compare_songs()
-    # write_song_to_file()
+    create_files_dirs()
+    song_check_loop()
 
 
 if __name__ == '__main__':
